4546a4a05ab8e87c10021f415d291f880dcfd847
Yentl-GV <106481920+Yentl-GV@users.noreply.github.com>
2022-05-29 17:44:57 +0200

    Add files via upload

---
+++ practica-gomez.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *@author Yentl
+ *@version 1.0
+ *{@internal He creado una nueva función de resta "function resta" ahora tenemos dos funciones.}
+*/
+
+/**
+ *Función suma de dos números
+ *@param int $num1
+ *@param int $num2
+ *@return float
+*/
+
+function suma ($num1, $num2){
+ $resultadosuma=$num1+$num2;
+ return $resultadosuma;
+}
+
+$suma= suma(24,42);
+echo $suma;
+
+/**
+ *Función resta de dos numeros
+ *@param int $num3
+ *@param int $num4
+ *@return float
+*/
+function resta ($num3,$num4){
+ $resultadoresta=$num3-$num4;
+ return $resultadoresta;
+}
+
+$resta= resta(10,5);
+echo $resta;
+?>
